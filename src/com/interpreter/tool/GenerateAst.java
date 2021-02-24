d52f234b1e4717eca6fea92f7713f173d418ee8d
codert <codert.sn@gmail.com>
2021-02-24 20:00:01 +0800

    feat: generate Expr.java

--- src/com/interpreter/tool/GenerateAst.java
+++ src/com/interpreter/tool/GenerateAst.java
@@ -27,7 +27,7 @@ public class GenerateAst {
         String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.craftinginterpreters.lox;");
+        writer.println("package com.interpreter.lox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
