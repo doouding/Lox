edf06a4c603307443893c36ff6de13c1ce639bc2
codert <codert.sn@gmail.com>
2021-02-24 22:34:56 +0800

    feat: add visitor pattern

--- src/com/interpreter/lox/Expr.java
+++ src/com/interpreter/lox/Expr.java
@@ -3,11 +3,23 @@ package com.interpreter.lox;
 import java.util.List;
 
 abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
     }
 
     final Expr left;
@@ -19,11 +31,21 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
     final Expr expression;
   }
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
     }
 
     final Object value;
@@ -34,7 +56,14 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
     final Token operator;
     final Expr right;
   }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
