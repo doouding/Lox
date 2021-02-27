04ab126424d558db4759164d3656bbab40494454
codert <codert.sn@gmail.com>
2021-02-27 22:49:13 +0800

    feat: add parser

--- src/com/interpreter/lox/Lox.java
+++ src/com/interpreter/lox/Lox.java
@@ -35,6 +35,14 @@ public class Lox {
         report(line, "", message);
     }
 
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     private static void report(int line, String where, String message) {
         System.err.println(
             "[line " + line + "] Error" + where + ": " + message
@@ -45,6 +53,11 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
 
         for(Token token: tokens) {
             System.out.println(token);
