bea94105959de2852992f2c774f23eadd3f8a8d4
小新 <codert.sn@gmail.com>
2021-03-02 05:04:48 +0000

    feat: add literal and grouping expr

--- src/com/interpreter/lox/Interpreter.java
+++ src/com/interpreter/lox/Interpreter.java
@@ -1,5 +1,17 @@
 package com.interpreter.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
 
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
 }
