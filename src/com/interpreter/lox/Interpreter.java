2fc50f4249cac33ef60a29450a49583f7202c97a
codert <codert.sn@gmail.com>
2021-03-03 23:54:56 +0800

    feat: add evaluate binary expr to interpreter

--- src/com/interpreter/lox/Interpreter.java
+++ src/com/interpreter/lox/Interpreter.java
@@ -14,4 +14,72 @@ class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return -(double)right;
+            case BANG:
+                return !isTruthy(right);
+            default:
+                return null;
+        }
+    }
+
+    public boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            default:
+                return null;
+        }
+
+        return null;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null & b == null) return true;
+        if (a == null) return false;
+
+        return a.equals(b);
+    }
 }
