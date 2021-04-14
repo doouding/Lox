d3369ce52795bf50475af4b9b1ebe675054a12e2
codert <codert.sn@gmail.com>
2021-04-14 22:06:02 +0800

    fix: set statement

--- src/com/interpreter/lox/Parser.java
+++ src/com/interpreter/lox/Parser.java
@@ -252,7 +252,7 @@ public class Parser {
             } if (expr instanceof Expr.Get) {
                 Expr.Get get = (Expr.Get)expr;
 
-                return new Expr.Set(get.object, equals, value);
+                return new Expr.Set(get.object, get.name, value);
             }
 
             error(equals, "Invalid assignment target.");
