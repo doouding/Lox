f0065be81ce743c15086121b44f9d2f8006d01a5
codert <codert.sn@gmail.com>
2021-04-25 23:25:15 +0800

    feat: add lox instance proxy

---
+++ src/com/interpreter/lox/LoxInstanceProxy.java
@@ -0,0 +1,24 @@
+package com.interpreter.lox;
+
+public class LoxInstanceProxy {
+    private LoxInstance instance;
+    private boolean allowPrivate;
+
+    LoxInstanceProxy(LoxInstance instance, boolean allowPrivate) {
+        this.instance = instance;
+        this.allowPrivate = allowPrivate;
+    }
+
+    void set(Token name, Object value) {
+        this.instance.set(name, value, this.allowPrivate);
+    }
+
+    Object get(Token name) {
+        return this.instance.get(name, this.allowPrivate);
+    }
+
+    @Override
+    public String toString() {
+        return this.instance.toString();
+    }
+}
