69a93a6843f594071c7c00aed7786cc12270eec5
codert <codert.sn@gmail.com>
2021-08-19 22:10:54 +0800

    chore: clean

--- src/main/java/org/doouding/lox/Lox.java
+++ src/main/java/org/doouding/lox/Lox.java
@@ -1,9 +1,6 @@
 package org.doouding.lox;
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
